ad9b2f2ba916e5e0a08ed4b62163241a0d3ffdd8
raggof <mwalberto11@gmail.com>
2021-04-23 13:31:03 +0200

    #4-All-controllers-using-Services-instead-Repo-Delete-unnecessary-imports

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.entity.Institution;
-import pl.coderslab.repositories.DonationRepository;
-import pl.coderslab.repositories.InstitutionRepository;
 import pl.coderslab.services.DonationService;
 import pl.coderslab.services.InstitutionService;
 
